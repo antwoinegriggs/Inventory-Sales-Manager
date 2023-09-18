ae875972f5643d5c7dfac87ab0d2b901e64e5d3e
antwoine <antwoinegriggs@outlook.com>
2023-09-18 03:53:21 -0400

    seed update, customer + inventory

--- server/seed.py
+++ server/seed.py
@@ -1,4 +1,5 @@
-from random import choice as rc
+from random import choice
+from random import randint
 
 from faker import Faker
 
@@ -13,8 +14,23 @@ def create_customer():
     return Customer(name=fake.first_name()+' '+fake.last_name())
 
 
+def create_inventory():
+    return ProductInventory(
+        product_name=fake.word(),
+        product_number=randint(1000, 9999),
+        product_quantity=randint(1, 100),
+        product_price=randint(1, 100)
+    )
+
+
 if __name__ == '__main__':
     with app.app_context():
+        # customer
         customers = [create_customer() for _ in range(5)]
         db.session.add_all(customers)
+
+        # inventory
+        products = [create_inventory() for _ in range(10)]
+        db.session.add_all(products)
+
         db.session.commit()
